c854019df9f7c4ee914a6ac47a8bf072d942c888
TerminiUsMag <nikolaidan1@abv.bg>
2023-05-08 17:13:36 +0300

    Added summaries to IGenreService and GenreService classes;

--- LibraryManagerConsole.Core/Contracts/IGenreService.cs
+++ LibraryManagerConsole.Core/Contracts/IGenreService.cs
@@ -5,10 +5,62 @@ namespace LibraryManagerConsole.Core.Contracts
 {
     public interface IGenreService
     {
-        Task AddGenre();
+
+        /// <summary>
+        /// Creates a new Genre Model if there's no such genre in Database
+        /// </summary>
+        /// <param name="genreName">Name of the new genre</param>
+        /// <returns>Genre Model</returns>
+        /// <exception cref="ArgumentException"></exception>
         Task<GenreModel> CreateGenreModel(string genreName);
+
+        /// <summary>
+        /// Transfers genres from Book Model to Book (checks for existing genres in DB)
+        /// </summary>
+        /// <param name="book"></param>
+        /// <param name="bookModel"></param>
+        /// <returns>Book</returns>
         Task<Book> ExistingGenresFromBookModelToBook(Book book, BookModel bookModel);
+
+        /// <summary>
+        /// Returns Genre Model with specific name from Book Model
+        /// </summary>
+        /// <param name="bookModel">Book Model to search for genre</param>
+        /// <param name="genreName">Genre name to find</param>
+        /// <returns>Genre Model</returns>
+        /// <exception cref="ArgumentException"></exception>
         GenreModel FindGenreInBookModel(BookModel bookModel, string genreName);
+
+        /// <summary>
+        /// Find Genre in Database and return it as Genre Model
+        /// </summary>
+        /// <param name="genreName">Name of Genre to find</param>
+        /// <returns>Genre Model</returns>
+        /// <exception cref="ArgumentException"></exception>
         Task<GenreModel> FindGenreAsync(string genreName);
+
+        /// <summary>
+        /// Adds a new Genre to DB
+        /// </summary>
+        /// <param name="genreName">Name of the new Genre</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        Task AddGenreAsync(string genreName);
+
+        /// <summary>
+        /// Adds a new Genre to DB
+        /// </summary>
+        /// <param name="genreModel">Genre Model of the new Genre</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        Task AddGenreAsync(GenreModel genreModel);
+
+        /// <summary>
+        /// Adds a new Genre to DB
+        /// </summary>
+        /// <param name="genre">new Genre</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        Task AddGenreAsync(Genre genre);
     }
 }
--- LibraryManagerConsole.Core/Services/GenreService.cs
+++ LibraryManagerConsole.Core/Services/GenreService.cs
@@ -19,6 +19,13 @@ namespace LibraryManagerConsole.Core.Services
             this.writer = _writer;
             this.reader = _reader;
         }
+
+        /// <summary>
+        /// Creates a new Genre Model if there's no such genre in Database
+        /// </summary>
+        /// <param name="genreName">Name of the new genre</param>
+        /// <returns>Genre Model</returns>
+        /// <exception cref="ArgumentException"></exception>
         public async Task<GenreModel> CreateGenreModel(string genreName)
         {
             if (genreName.IsNullOrEmpty())
@@ -37,6 +44,13 @@ namespace LibraryManagerConsole.Core.Services
                 return new GenreModel { Id = genre.Id, Name = genre.Name };
             }
         }
+
+        /// <summary>
+        /// Transfers genres from Book Model to Book (checks for existing genres in DB)
+        /// </summary>
+        /// <param name="book"></param>
+        /// <param name="bookModel"></param>
+        /// <returns>Book</returns>
         public async Task<Book> ExistingGenresFromBookModelToBook(Book book, BookModel bookModel)
         {
             var existingGenres = await repo.All<Genre>().ToListAsync();
@@ -66,6 +80,14 @@ namespace LibraryManagerConsole.Core.Services
             writer.WriteLine($"Existing Genres added to book : {existingGenresCounter}");
             return book;
         }
+
+        /// <summary>
+        /// Returns Genre Model with specific name from Book Model
+        /// </summary>
+        /// <param name="bookModel">Book Model to search for genre</param>
+        /// <param name="genreName">Genre name to find</param>
+        /// <returns>Genre Model</returns>
+        /// <exception cref="ArgumentException"></exception>
         public GenreModel FindGenreInBookModel(BookModel bookModel, string genreName)
         {
             if (genreName.IsNullOrEmpty())
@@ -81,6 +103,13 @@ namespace LibraryManagerConsole.Core.Services
             }
             return genreModel;
         }
+
+        /// <summary>
+        /// Find Genre in Database and return it as Genre Model
+        /// </summary>
+        /// <param name="genreName">Name of Genre to find</param>
+        /// <returns>Genre Model</returns>
+        /// <exception cref="ArgumentException"></exception>
         public async Task<GenreModel> FindGenreAsync(string genreName)
         {
             if (genreName.IsNullOrEmpty())
@@ -100,9 +129,59 @@ namespace LibraryManagerConsole.Core.Services
                 Id = genre.Id,
             };
         }
-        public Task AddGenre()
+
+        /// <summary>
+        /// Adds a new Genre to DB
+        /// </summary>
+        /// <param name="genreName">Name of the new Genre</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task AddGenreAsync(string genreName)
         {
-            throw new NotImplementedException();
+            if (genreName.IsNullOrEmpty())
+            {
+                throw new ArgumentException("Required argument is null or empty");
+            }
+
+            var genreInDb = await repo
+                .All<Genre>()
+                .Where(g=>g.Name==genreName)
+                .FirstOrDefaultAsync();
+
+            if (genreInDb is not null)
+            {
+                throw new ArgumentException("There's already a genre with the same name in the DB");
+            }
+
+            var newGenre = new Genre
+            {
+                Name = genreName,
+            };
+
+            await repo.AddAsync(newGenre);
+            await repo.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Adds a new Genre to DB
+        /// </summary>
+        /// <param name="genreModel">Genre Model of the new Genre</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task AddGenreAsync(GenreModel genreModel)
+        {
+            await AddGenreAsync(genreModel.Name);
+        }
+
+        /// <summary>
+        /// Adds a new Genre to DB
+        /// </summary>
+        /// <param name="genre">new Genre</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task AddGenreAsync(Genre genre)
+        {
+            await AddGenreAsync(genre.Name);
         }
     }
 }
